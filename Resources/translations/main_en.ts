e26cba0b7859ba5c34995e4e09951ad1098aa99c
PowerSnail <hj5fb@virginia.edu>
2019-11-10 22:58:57 -0800

    translatioon

---
+++ Resources/translations/main_en.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>ColorChooser</name>
+    <message>
+        <location filename="../../src/widgets/color_chooser.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/color_chooser.cpp" line="31"/>
+        <source>Choose a color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="../../src/views/dialogabout.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/dialogabout.ui" line="26"/>
+        <source>&lt;h2 id=&quot;randot-studio&quot;&gt;Randot Studio&lt;/h2&gt;
+&lt;p&gt;A program to create stereo images.&lt;/p&gt;
+&lt;h2 id=&quot;acknowledgment&quot;&gt;Acknowledgment&lt;/h2&gt;
+&lt;p&gt;Icons made by &lt;a href=&quot;https://www.flaticon.com/authors/smashicons&quot; title=&quot;Smashicons&quot;&gt;Smashicons&lt;/a&gt; from &lt;a href=&quot;https://www.flaticon.com/&quot; title=&quot;Flaticon&quot;&gt;www.flaticon.com&lt;/a&gt;&lt;/p&gt;
+&lt;p&gt;Icons made by &lt;a href=&quot;https://www.flaticon.com/authors/freepik&quot; title=&quot;Freepik&quot;&gt;Freepik&lt;/a&gt; from &lt;a href=&quot;https://www.flaticon.com/&quot; title=&quot;Flaticon&quot;&gt;www.flaticon.com&lt;/a&gt;&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="37"/>
+        <source>Image Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="67"/>
+        <source>Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="87"/>
+        <source>Height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="97"/>
+        <source>Grain Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="107"/>
+        <source>Crossed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="124"/>
+        <source>Foreground</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="144"/>
+        <source>Background</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="174"/>
+        <source>Targets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="253"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="258"/>
+        <source>90</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="263"/>
+        <source>180</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="268"/>
+        <source>270</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="285"/>
+        <source>x</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="304"/>
+        <source>Rotate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="321"/>
+        <source>Target</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="396"/>
+        <source>Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="412"/>
+        <source>y</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="429"/>
+        <source>Shape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="445"/>
+        <source>Parity</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="458"/>
+        <source>Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="493"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="526"/>
+        <source>Randot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="531"/>
+        <source>Stereo PNG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="536"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="541"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.ui" line="550"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.cpp" line="146"/>
+        <source>Choose shape images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.cpp" line="162"/>
+        <source>Cannot delete E and C shapes.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/views/mainwindow.cpp" line="335"/>
+        <location filename="../../src/views/mainwindow.cpp" line="341"/>
+        <location filename="../../src/views/mainwindow.cpp" line="347"/>
+        <location filename="../../src/views/mainwindow.cpp" line="353"/>
+        <source>Choose file to save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
