cd3e87c5f4d67f1fb120f85866d2287ea4620dae
powersnail <hj5fb@virginia.edu>
2019-11-11 12:52:47 -0800

    windows installer

--- installer/make_win_installer.bat
+++ installer/make_win_installer.bat
@@ -3,13 +3,21 @@
 :: 2. Make sure Qt executables are in PATH
 :: 3. Make sure QtInstallerFramework executables are in PATH
 
-SET RELEASE_DIR="build\release"
-SET BUILD_INSTALLER_DIR="build\installer\randotstudio"
-SET INSTALLER_DESTINATION="installer\packages\com.powersnail.randotstudio\data"
-SET INSTALLER_OUTPUT_DIR="build\installer"
+@echo off
+SET RELEASE_DIR=build\release
+SET BUILD_INSTALLER_DIR=build\installer\randotstudio
+SET INSTALLER_DESTINATION=installer\packages\com.powersnail.randotstudio\data
+SET INSTALLER_OUTPUT_DIR=build\installer
 
 mkdir %BUILD_INSTALLER_DIR%
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 copy %RELEASE_DIR%\RandotStudio.exe %BUILD_INSTALLER_DIR%
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 windeployqt.exe %BUILD_INSTALLER_DIR%\RandotStudio.exe
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 archivegen.exe %INSTALLER_DESTINATION%\randotstudio.7z %BUILD_INSTALLER_DIR%
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 binarycreator.exe -p .\installer\packages -c .\installer\config\config.xml %INSTALLER_OUTPUT_DIR%\installer.exe
+if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
+
+echo The installer has been created in %INSTALLER_OUTPUT_DIR%
